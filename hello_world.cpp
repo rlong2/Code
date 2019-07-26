aa3f146d552b1a2eaab785c3706ebb0630106ce3
Ryan Long <rlong2@protonmail.com>
2019-07-26 13:31:15 -0600

    adding hello world program

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+// This program shows how to send text output to the console
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Hello world!" << std::endl;
+    return 0;
+} // main
