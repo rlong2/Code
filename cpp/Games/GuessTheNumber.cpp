746361bd14920b153436df5a50a654c99878982e
rlong2 <53006195+rlong2@users.noreply.github.com>
2019-07-31 08:45:05 -0600

    Created Games directory and GuessTheNumber.cpp
    From 'Beginning C++ through Game Programming'

---
+++ cpp/Games/GuessTheNumber.cpp
@@ -0,0 +1,41 @@
+// Guess My Number (1 - 100)
+// Modified from Beginning C++ Through Game Programming, 4th Edition
+// http://www.delmarlearning.com/companions/content/1305109910/datafiles/index.asp?isbn=1305109910
+// Random number generation: http://www.cplusplus.com/reference/cstdlib/rand/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    srand (time(NULL)); // Generate random seed
+    int number = rand() % 100 + 1; // generate random number
+    int guess = 0;
+    int tries = 0;
+    
+    do
+    {
+        cout << "Enter your guess: " ;
+        cin >> guess;
+        
+        if (guess < number)
+        {
+            cout << "Too low!" << endl;
+            tries++;
+        }
+        else if (guess > number)
+        {
+            cout << "Too high!" << endl;
+            tries++;
+        }
+        else
+        {
+            cout << "You guessed the random number in " << tries << " tries!" << endl;
+        }
+    } while (guess != number);
+    
+    return 0;
+}
