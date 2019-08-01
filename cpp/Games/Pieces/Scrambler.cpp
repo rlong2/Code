a82e19b252a7e86bf829eea200293c12330dc9f2
rlong2 <53006195+rlong2@users.noreply.github.com>
2019-08-01 10:52:28 -0600

    Update Scrambler.cpp
    Words are now randomly scrambled

--- cpp/Games/Pieces/Scrambler.cpp
+++ cpp/Games/Pieces/Scrambler.cpp
@@ -1,4 +1,4 @@
-// Word Scramble
+// Word Scramble - Scrambles a given string
 // https://www.geeksforgeeks.org/shuffle-vs-random_shuffle-c/
 
 #include <iostream>
@@ -8,10 +8,19 @@ using namespace std;
 
 string scrambledWord = "";
 
-string Scramble(string word)
+string Scramble(string word, int length)
 {
-    word = scrambledWord;
-    scrambledWord = "gregreafcdg";
+    scrambledWord = word;
+
+    for (int i = 0; i < length; i++)
+    {
+        int index1 = (rand() % length);
+        int index2 = (rand() % length);
+        char temp = scrambledWord[index1];
+        scrambledWord[index1] = scrambledWord[index2];
+        scrambledWord[index2] = temp;
+    }
+    
     return scrambledWord;
 }
 
@@ -21,10 +30,12 @@ int main()
     string word = "";
     cin >> word;
     
-    Scramble(word);
+    int length = word.length();
     
-    cout << "original word: " << word;
-    cout << "scrambled word: " << scrambledWord;
+    Scramble(word, length);
+    
+    cout << "original word: " << word << endl;
+    cout << "scrambled word: " << scrambledWord << endl;
     
      return 0;  
      
