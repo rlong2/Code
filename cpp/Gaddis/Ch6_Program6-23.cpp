61a113d3e198015f90feaa129c3bc80d074e8680
rlong2 <53006195+rlong2@users.noreply.github.com>
2019-08-07 08:28:39 -0600

    Create Ch6_Program6-23.cpp

---
+++ cpp/Gaddis/Ch6_Program6-23.cpp
@@ -0,0 +1,31 @@
+/*  This program uses a reference variable as a function parameter.
+* Reference variables are defined like regular variables,
+* except there is an ampersand (&) between the data type and name.
+* The & must appear in both the prototype and header.
+* The & does not appear in a function call.
+*/
+
+#include <iostream>
+using namespace std;
+
+// Function prototype. The parameter is a reference variable
+void DoubleNum(int &refVar);
+
+int main()
+{
+    int num = 5;
+    
+    cout << "In main, num is " << num << endl;
+    
+    DoubleNum(num);
+    
+    cout << "Back in main, num is " << num << endl;
+    
+    return 0;
+}
+
+// Note that this function is void!
+void DoubleNum(int &refVar)
+{
+    refVar = refVar * 2;  // doubles the value in main
+}
