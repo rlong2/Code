46d6e41ffb2957152af239d19399a30dd63f8e71
Ryan Long <rlong2@protonmail.com>
2019-08-11 08:24:10 -0600

    adding rust directory

---
+++ rust/rustByExample/README_RBE.md
@@ -0,0 +1,1 @@
+These exercises are from Rust's site here: https://doc.rust-lang.org/stable/rust-by-example/
---
+++ rust/rustByExample/hello.rs
@@ -0,0 +1,7 @@
+// This is Hello World written in Rust
+
+fn main()
+{
+    println!("Hello World!");
+    println!("I'm a Rustacean!");
+}
