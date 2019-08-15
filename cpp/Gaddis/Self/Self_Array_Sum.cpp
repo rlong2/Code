f3f0f48dd0e7f8505e37991475774514d08a47d7
rlong2 <53006195+rlong2@users.noreply.github.com>
2019-08-15 12:54:59 -0600

    Update Self_Array_Sum.cpp
    Added code for finding the average value in the array

--- cpp/Gaddis/Self/Self_Array_Sum.cpp
+++ cpp/Gaddis/Self/Self_Array_Sum.cpp
@@ -1,4 +1,5 @@
 // This program sums the contents of an array
+// and finds the average value.
 
 #include <iostream>
 using namespace std;
@@ -8,13 +9,17 @@ int main()
     const int SIZE = 6;
     int arrayNums[SIZE] = { 5, 23, 8, 57, 99, 123 };
     int total = 0;
+    int average;
     
     for (int i = 0; i < SIZE; i++)
     {
         total += arrayNums[i];
     }
     
-    cout << "The array's total is: " << total;
+    average = total / SIZE;
+    
+    cout << "The array's total is " << total << endl;
+    cout << "The average value is " << average << endl;
     
     return 0;
 }
