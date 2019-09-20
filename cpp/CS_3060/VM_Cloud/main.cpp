00486010d11ee7d8c690681641235494d98aba20
rlong2 <53006195+rlong2@users.noreply.github.com>
2019-09-20 13:32:10 -0600

    Update main.cpp

--- cpp/CS_3060/VM_Cloud/main.cpp
+++ cpp/CS_3060/VM_Cloud/main.cpp
@@ -19,6 +19,11 @@
 */
 
 #include <iostream>
+#include <iomanip>
+#include <math.h>     // for rounding up number of users
+
+#define GET_COST(map, hr) (map[hr - 6])
+
 using namespace std;
 
 enum TimeZone{
@@ -30,48 +35,39 @@ enum TimeZone{
     MAX_TZ
 };
 
-// Function prototype
-void currentUsage(TimeZone tz, double costPerHour);
+enum Direction { BOTH, LEFT, RIGHT };
+float mapPercentage[]{ .5, .75, 1, .75, .5 };
 
-float mapPercentage[]{
-    .5, .75, 1, .75, .5
-};
-
-int TotalMonies(int initialUsers, int direction, int currTime, TimeZone currentZone){
-    //if(currTime <= 5 || currTime >= 11 || currentZone >= MAX_TZ || currentZone <= NO_TZ) return 0;
+int TotalUsers(int initialUsers, Direction direction, int currTime, TimeZone currentZone){
     
     if(currentZone >= MAX_TZ || currentZone <= NO_TZ) // base case
         return 0;
         
-    int ret = mapPercentage[currTime - 6] * initialUsers;
+    int totalUsers = GET_COST(mapPercentage, currTime) * initialUsers;
     
-    //direction = 0: both ways
-    //direction = 1: left
-    //direction = 2: right
     switch(direction){
-        case 0:
-            ret += TotalMonies(initialUsers, 1, currTime - 1, (TimeZone)(currentZone - 1));
-            ret += TotalMonies(initialUsers, 2, currTime + 1, (TimeZone)(currentZone + 1));
-            return ret;
-        case 1:
-            ret += TotalMonies(initialUsers, 1, currTime - 1, (TimeZone)(currentZone - 1));
-            return ret;
-        case 2:
-            ret += TotalMonies(initialUsers, 2, currTime + 1, (TimeZone)(currentZone + 1));
-            return ret;
+        case Direction::BOTH:
+            totalUsers += TotalUsers(initialUsers, Direction::LEFT,  currTime - 1, (TimeZone)(currentZone - 1));
+            totalUsers += TotalUsers(initialUsers, Direction::RIGHT, currTime + 1, (TimeZone)(currentZone + 1));
+            return totalUsers;
+        case Direction::LEFT:
+            totalUsers += TotalUsers(initialUsers, Direction::LEFT, currTime - 1,  (TimeZone)(currentZone - 1));
+            return totalUsers;
+        case Direction::RIGHT:
+            totalUsers += TotalUsers(initialUsers, Direction::RIGHT, currTime + 1, (TimeZone)(currentZone + 1));
+            return totalUsers;
         default:
             break;
     }
     
-    return ret;
+    return totalUsers;
 }
 
 int main()
 {
     TimeZone tz;
     double costPerHour;
-
-    cout << "This program calculates VM stuff." << endl;
+    const int  VM_CAPACITY = 150;
 
     cout << "Assume it is 8PM local time." << endl;
     cout << "Enter the time zone you're in:" << endl;
@@ -88,53 +84,15 @@ int main()
     cout << "\nEnter the cost of VM per hour. Ex, .10): ";
     cin  >> costPerHour;
 
-    int totalUsers = TotalMonies(500000, 0, 8, tz);
-    std:cout << totalUsers << "\n";
-    // currentUsage(tz, costPerHour);
-
-    return 0;
-}
-
-// Function implementation
-void currentUsage(TimeZone tz, double costPerHour)
-{
-    const int PEAK = 500000;                      // Peak number of users at 8PM
-    const double REDUCTION = 0.75;               // At 7 and 9 PM, reduce load by 25%
-    const int  VM_CAPACITY = 150;
-    int totalVms;
-    int users;
-    double totalCost;
-
-    if (tz == PACIFIC || tz == MOUNTAIN)
-        cout << "\nYou are connected to the West datacenter." << endl;
-    else
-        cout << "\nYou are connected to the East datacenter." << endl;
-
-
-    // Get one time zone cost
-    users =  PEAK;            // 8PM
-
-    if (tz - 1 <= NO_TZ)
-    {
-        cout << "I'm in pacific" << endl;
-        users += PEAK * REDUCTION;        // Accounts for C time at 9PM
-    }
+    int users = TotalUsers(500000, Direction::BOTH, 8, tz);
     
-    
-    else if (tz + 1 >= MAX_TZ)
-    {
-        cout << "I'm in eastern" << endl;
-    }
-    
-    // Add users in other time zones
-    users += PEAK * REDUCTION;
-
-    totalVms = users / VM_CAPACITY;
-    totalCost = totalVms * costPerHour;
+    int totalVms = ceil(users / (float)VM_CAPACITY);
+    float totalCost = totalVms * costPerHour;
 
     cout << "\ntotal users in time zone: " << users << endl;
     cout << "Total VMs needed: " << totalVms << endl;
+    cout << fixed << setprecision(2);
     cout << "Total cost is $" << totalCost << endl;
 
-
+    return 0;
 }
