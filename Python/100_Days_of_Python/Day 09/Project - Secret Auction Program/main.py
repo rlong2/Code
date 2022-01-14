5426e2dad39a878a87731158d73ac3f9508f62dd
rlong2 <rlong2@protonmail.com>
2022-01-14 12:46:59 -0700

    Finished function to add bids to a list

--- Python/100_Days_of_Python/Day 09/Project - Secret Auction Program/main.py
+++ Python/100_Days_of_Python/Day 09/Project - Secret Auction Program/main.py
@@ -1,25 +1,34 @@
-from replit import clear # HINT: You can call clear() to clear the output in the console.
+#from replit import clear # HINT: You can call clear() to clear the output in the console.
 import art
 
 # Print ascii art and intoduction
 print(art.logo)
 print("Welcome to the secret auction program.")
 
-# TODO: Function to get highest bid
-
-
+# Function to add name of bidder and bid amount to a list
+def addBidToList(name, bid):
+    new_bid = {}
+    new_bid["name"] = name
+    new_bid["bid"] = bid
+    bid_list.append(new_bid)
+    
 # Get player names and bids
 # If there are multiple bidders, clear the screen
 # and reprompt for each name and bid.
+bid_list = [] # An empty list of dictionaries
+
 while True:
     name = input("What is your name?: ")
-    bid = input("What is your bid?: $")
+    bid = int(input("What is your bid?: $"))
+    addBidToList(name, bid)
+
     more_bidders = input("Any other bidders? Enter yes or no. ")
     if more_bidders.lower().startswith('y'):
-        clear()
+        # Continue loop to add bidders
+        pass
     else:
+        # Get highest bid and name of bidder
         break
 
-print(f"The winnder is {name} with a bid of ${bid}.")
-
-# The winnder is <name> with a bid of $bid
+print(bid_list)
+#print(f"The winnder is {name} with a bid of ${bid}.")
