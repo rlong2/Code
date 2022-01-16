f1144baa054f89286d518d7700e181ece2e493f1
rlong2 <rlong2@protonmail.com>
2022-01-15 17:10:45 -0700

    Secret auction program complete

--- Python/100_Days_of_Python/Day 09/Project - Secret Auction Program/Project - Secret Auction Program.py
+++ Python/100_Days_of_Python/Day 09/Project - Secret Auction Program/Project - Secret Auction Program.py
@@ -1,43 +1,32 @@
-#from replit import clear # HINT: You can call clear() to clear the output in the console.
+from replit import clear # HINT: You can call clear() to clear the output in the console.
 import art
 
 # Print ascii art and intoduction
 #print(art.logo)
 print("Welcome to the secret auction program.")
 
-# Function to add name of bidder and bid amount to a list
-def addBidToList(name, bid):
-    new_bid = {}
-    new_bid["name"] = name
-    new_bid["bid"] = bid
-    bid_list.append(new_bid)
-
-# Function to find the highest bid from the list
-def getHighestBid(bid_list):
-    highest_bid = 0
-    for dictionary in bid_list:
-        for entry in dictionary:
-            if dictionary[entry] > highest_bid:
-                highest_bid = dictionary[entry]
-    print(f"Highest bid is {highest_bid}")
-    
-# Get player names and bids
-# If there are multiple bidders, clear the screen
-# and reprompt for each name and bid.
-bid_list = [] # An empty list of dictionaries
+# Main
+# Get player names and bids.
+bid_list = {} # An empty dictionary to hold bids
 
 while True:
     name = input("What is your name?: ")
     bid = int(input("What is your bid?: $"))
-    addBidToList(name, bid)
+    bid_list.update({name: bid})
 
     more_bidders = input("Any other bidders? Enter yes or no. ")
     if more_bidders.lower().startswith('y'):
-        # Continue loop to add bidders
-        pass
+        # Continue loop to add bidders, clearing the screen each time.
+        clear()
     else:
-        # Get highest bid and name of bidder
-        getHighestBid(bid_list)
+        # Auction over, find the bidder with the highest bid
+        highest_bid = 0
+        highest_bidder = "default"
+        for entry in bid_list:
+            print(entry, bid_list[entry])
+            if bid_list[entry] > highest_bid:
+                highest_bid = bid_list[entry]
+                highest_bidder = entry
         break
 
-#print(f"The winnder is {name} with a bid of ${bid}.")
+print(f"The winnder is {highest_bidder} with a bid of ${highest_bid}.")
