c7ea40fae323592d07666cf56377bb051d58794a
rlong2 <rlong2@protonmail.com>
2022-01-16 09:43:08 -0700

    Update MouseClick.py
    Added examples and regex to check user input

--- Python/AutomateTheBoringStuff/MouseClick.py
+++ Python/AutomateTheBoringStuff/MouseClick.py
@@ -6,15 +6,26 @@ https://pypi.org/project/mouse/
 https://stackoverflow.com/questions/11523918/python-start-a-function-at-given-time
 '''
 
-import schedule, time, mouse
+import schedule, time, mouse, re
 
 def MouseClick():
     print("Click!")
     mouse.click('left')
 
+def Examples():
+    print('''Enter the time you want mouse to click in a XX:XX or XX:XX:XX format.
+Example: If you want 9 AM, enter 09:00
+Example: If you want 1:20 PM + 40 seconds, enter 13:20:40
+          ''')
+    
 # Main
-#schedule.every(10).seconds.do(MouseClick)
-schedule.every().day.at("13:44").do(MouseClick)
+Examples()
+user_time = "1"
+while not re.match("[0-2][0-9]:[0-9][0-9]", user_time):
+    user_time = input("Time for mouse click: ")
+
+print(f"Mouse set to click at {user_time}")           
+schedule.every().day.at(user_time).do(MouseClick)
 
 while True:
     schedule.run_pending()
