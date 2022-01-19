14c493c20ff3ea78e536c3785f6a48113f1017d3
rlong2 <rlong2@protonmail.com>
2022-01-18 20:25:10 -0700

    Added comments for clarity

--- Python/100_Days_of_Python/Day 10/Project - Calculator/Project - Calculator.py
+++ Python/100_Days_of_Python/Day 10/Project - Calculator/Project - Calculator.py
@@ -31,9 +31,11 @@ num2 = int(input("What's the second number?: "))
 
 for symbol in operations:
     print(symbol)
-
+    
 operation_symbol = input("Pick an operation from the line above: ")
 calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
+
+# Call the appropriate function ansd store it (For example, add(3, 4), multiply(6, 7))
+answer = calculation_function(num1, num2)   
 
 print(f"{num1} {operation_symbol} {num2} = {answer}")
