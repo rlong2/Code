8ddf66d622b1973fa4057c533f3d1fcc73e51869
rlong2 <rlong2@protonmail.com>
2022-01-19 19:28:12 -0700

    Calculator can chain commands

--- Python/100_Days_of_Python/Day 10/Project - Calculator/Project - Calculator.py
+++ Python/100_Days_of_Python/Day 10/Project - Calculator/Project - Calculator.py
@@ -29,12 +29,23 @@ operations = {
 num1 = int(input("What's the first number?: "))
 for symbol in operations:
     print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("What's the second number?: "))
 
-calculation_function = operations[operation_symbol]
+should_continue = True
 
-# Call the appropriate function and store it (For example, add(3, 4), multiply(6, 7))
-answer = calculation_function(num1, num2)   
+while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = int(input("What's the next number?: "))
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+    calculation_function = operations[operation_symbol]
+
+    # Call the appropriate function and store it (For example, add(3, 4), multiply(6, 7))
+    answer = calculation_function(num1, num2)   
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit: ").lower() == 'y':
+        num1 = answer
+    else:
+        should_continue = False
+
+print("\nThanks for using the calculator! Bye!")
