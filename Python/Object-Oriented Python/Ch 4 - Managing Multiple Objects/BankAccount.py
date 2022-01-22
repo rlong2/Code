c2ff8d306191c53f91864696915a58dc778d6360
rlong2 <rlong2@protonmail.com>
2022-01-22 16:16:43 -0700

    Added code to deposit and get balance

--- Python/Object-Oriented Python/Ch 4 - Managing Multiple Objects/BankAccount.py
+++ Python/Object-Oriented Python/Ch 4 - Managing Multiple Objects/BankAccount.py
@@ -7,13 +7,31 @@ class Account():
         self.balance = int(balance)
 
     def deposit(self, amountToDeposit, password):
-        pass
+        if password == self.password:
+            if amountToDeposit < 0:
+                print("You cannot deposit a negative amount")
+                return None
+
+            self.balance = self.balance + amountToDeposit
+            return self.balance
+        
+        else:
+            print("Sorry, incorrect password!")
+            return None
 
     def withdraw(self, amountToWithdraw, password):
-        pass
+        if password == self.password:
+            pass
+        else:
+            print("Sorry, incorrect password!")
+            return None
 
     def getBalance(self, password):
-        pass
+        if password == self.password:
+            return self.balance
+        else:
+            print("Sorry, incorrect password!")
+            return None
 
     # Debugging
     def show(self):
