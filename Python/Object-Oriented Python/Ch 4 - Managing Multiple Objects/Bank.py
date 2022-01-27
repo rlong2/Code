23e7b67135c9435a9677bbba99dbac36f924e39e
rlong2 <rlong2@protonmail.com>
2022-01-26 19:27:06 -0700

    Created unimplemented Bank functions

--- Python/Object-Oriented Python/Ch 4 - Managing Multiple Objects/Bank.py
+++ Python/Object-Oriented Python/Ch 4 - Managing Multiple Objects/Bank.py
@@ -1,2 +1,31 @@
 # A Bank class that manages a dictionary of Account objects
 
+from Account import *
+
+class Bank():
+    def __init__(self):
+        self.accountsDict = {}
+        self.nextAccountNumber = 0
+
+    
+    def createAccount(self, name, startingAmount, password):
+        pass
+
+    def openAccount(self):
+        pass
+
+    def closeAccount(self):
+        pass
+
+    def balance(self):
+        pass
+
+    def deposit(self):
+        pass
+
+    def withdraw(self):
+        pass
+
+    def show(self):
+        pass
+
