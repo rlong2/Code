3a9b2ceb8fc4353c225b43f636a5f3dce5920841
rlong2 <rlong2@protonmail.com>
2022-01-28 16:53:16 -0700

    Added menu code for users

--- Python/Object-Oriented Python/Ch 4 - Managing Multiple Objects/Main_Bank_Version5.py
+++ Python/Object-Oriented Python/Ch 4 - Managing Multiple Objects/Main_Bank_Version5.py
@@ -8,4 +8,38 @@ oBank = Bank()
 # Main code
 # Create two test accounts
 larrysAccountNumber = oBank.createAccount("Larry", 1000, "larrypass")
-oBank.balance()
+ryansAccountNumber = oBank.createAccount("Ryan", 20, "ryanpass")
+
+# Loop through menu options
+while True:
+    print()
+    print("Press b to get the balance")
+    print("Press d to make a deposit")
+    print("Press o to open a new account")
+    print("Press c to close an existing account")
+    print("Press w to make a withdrawl")
+    print("Press s to show all accounts")
+    print("Press q to quit")
+    print()
+
+    action = input("What do you want to do? ")
+    action = action[0].lower() # Grab the first letter, make it lowercase
+    print()
+
+    if action == 'b':
+        oBank.balance()
+    elif action == 'd':
+        oBank.deposit()  
+    elif action == 'o':
+        oBank.openAccount() 
+    elif action == 'c':
+        oBank.closeAccount()
+    elif action == 'w':
+        oBank.withdraw()
+    elif action == 's':
+        oBank.show()
+    elif action == 'q':
+        print("*** Quitting... ***")
+        break
+    else:
+        print("Invaid input, try again.")
