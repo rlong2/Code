23fbca1b52a34f46a6683fcdaaba7b2e97574f32
rlong2 <rlong2@protonmail.com>
2022-01-28 17:14:35 -0700

    Adding method to validate account info (password and integer input)

--- Python/Object-Oriented Python/Ch 4 - Managing Multiple Objects/Account.py
+++ Python/Object-Oriented Python/Ch 4 - Managing Multiple Objects/Account.py
@@ -1,4 +1,10 @@
 # A bank account class. It can deposit, withdraw, and check status.
+# Errors will be caught by "raise" statements.
+
+# Custom exception
+class AbortTransaction(Exception):
+    '''raise this exception to abort a bank transaction'''
+    pass
 
 class Account():
     def __init__(self, name, balance, password):
@@ -6,43 +12,34 @@ class Account():
         self.balance = int(balance)
         self.password = password
 
-    def deposit(self, amountToDeposit, password):
-        if password == self.password:
-            if amountToDeposit < 0:
-                print("You cannot deposit a negative amount")
-                return None
+    def validateAmount(self, amount):
+        try:
+            amount = int(amount)
+        except ValueError:
+            raise AbortTransaction("Amount must be an integer.")
+        if amount <= 0:
+            raise AbortTransaction("Amount must be positive.")
+        return amount
 
-            self.balance = self.balance + amountToDeposit
-            print(f"After depositing ${amountToDeposit}, {self.name}'s account balance is ${self.balance}")
-            return self.balance
-        
-        else:
-            print("Sorry, incorrect password!")
-            return None
-
-    def withdraw(self, amountToWithdraw, password):
-        if password == self.password:
-            if amountToWithdraw < 0:
-                print("You cannot withdraw a negative amount")
-                return None
-            elif amountToWithdraw > self.balance:
-                print("You cannot withdraw more than you have in your account")
-                return None
-            else:
-                self.balance = self.balance - amountToWithdraw
-                print(f"After withdrawing ${amountToWithdraw}, {self.name}'s account balance is ${self.balance}")
-                return self.balance
-        else:
-            print("Sorry, incorrect password!")
-            return None
+    def checkPasswordMatch(self, password):
+        if password != self.password:
+            raise AbortTransaction("Incorrect password for this account.")
 
     def getBalance(self, password):
-        if password == self.password:
-            return self.balance
-        else:
-            print("Sorry, incorrect password!")
-            return None
+        return self.balance
+    
+    def deposit(self, amountToDeposit, password):
+        amountToDeposit = self.validateAmount(amountToDeposit)
+        self.balance += amountToDeposit
+        return self.balance
 
+    def withdraw(self, amountToWithdraw, password):
+        amountToWithdraw = self.validateAmount(amountToWithdraw)
+        if amountToWithdraw > self.balance:
+            raise AbortTransaction("You cannot withdraw more than you have in your account.")
+        self.balance = self.balance - amountToWithdraw
+        return self.balance
+        
     # Debugging
     def show(self):
         print(f"Name: {self.name}")
